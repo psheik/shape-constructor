7ecb27fad533d5c6b8edb40bd955e4672c1f33d8
Pasha Sheik <pashasheik33@gmail.com>
2019-09-10 19:48:55 -0700

    added a readme/cleaned up code/fixed inconsistencies in code/added notes to code

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# shape-constructor
+A set of functions which consists of general, rectangle, and square shape constructors----allowing for more general or specific shaping of some shapes
--- homework9.js
+++ homework9.js
@@ -1,26 +1,33 @@
+//General shape constructor
 function shapeConstructor(name, sides){
     this.name = name;
     this.sides = sides;
 }
 
+//General rectangle constructor 
 function rectangleConstructor(height, width, name, sides){
     this.height = height;
     this.width = width;
-    Shape.call(this, name, sides);
+    shapeConstructor.call(this, name, sides);
 }
 
+//designates rectangle prototype as a specific shape
 Rectangle.prototype = Object.create(Shape.prototype);
 
-RectangleConstructor.prototype.area = function(){
+//creates a prototype for the area of a rectangle
+rectangleConstructor.prototype.area = function(){
     console.log(this.height*this.width);
 }
 
-RectangleConstructor.prototype.perimeter = function(){
+//creates a prototype for the perimeter of a rectangle
+rectangleConstructor.prototype.perimeter = function(){
     console.log(this.height*2 + this.width*2);
 }
 
-function Square(height, name, sides){
+//General square constructor 
+function squareConstructor(height, name, sides){
     rectangleConstructor.call(this, height, width, name, sides);
 }
 
+//designates square prototype as a specific rectangle
 Square.prototype = object.create(Rectangle.prototype);
